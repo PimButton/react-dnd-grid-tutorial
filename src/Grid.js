54a3c9b499c11884810b8e57d4424c10d5c9bf4a
liekevdvoort <49523639+liekevdvoort@users.noreply.github.com>
2020-03-03 09:40:38 +0100

    Adjustable grid via comments

--- src/Grid.js
+++ src/Grid.js
@@ -2,14 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 export const Grid = styled.div`
-  width: 600px;
-  display: flex;
+  max-width:100vw;
+  width:auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  grid-gap: 10px;
   justify-content: start;
   flex-wrap: wrap;
 `;
 
 export const GridImage = styled.div`
-  flex-grow: 1;
+  width:100%;
+  height:auto;
   border: 1px solid white;
 
   display: flex;
@@ -22,20 +27,48 @@ export const GridImage = styled.div`
 `;
 
 const GridItemWrapper = styled.div`
-  flex: 0 0 33.3%;
   display: flex;
   justify-content: center;
   align-items: stretch;
-
+  align-self: stretch;
   box-sizing: border-box;
+
+  //option 1
+  // :nth-child(6){
+  //   grid-column: span 2; 
+  //   grid-row: span 2;
+  // }
+  // :nth-child(5){
+  //   grid-column: span 2;
+  //   grid-row: span 2;
+  // }
+
+  // //option 2
+  // :nth-child(2){
+  //   grid-column: span 3; 
+  //   grid-row: span 3;
+  // }
+  // :nth-child(5){
+  //   grid-column: span 2;
+  //   grid-row: span 2;
+  // }
+  //option repeat 
+  :nth-child(3n){
+    grid-column: span 2;
+    grid-row: span 2;
+  }
+
 
   :before {
     content: "";
     display: table;
     padding-top: 100%;
   }
+
 `;
 
 export const GridItem = ({ forwardedRef, ...props }) => (
   <GridItemWrapper ref={forwardedRef} {...props} />
 );
+
+
